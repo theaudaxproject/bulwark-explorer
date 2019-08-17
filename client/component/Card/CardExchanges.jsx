c48b31e1e19f589fdf769928d0e65d7d2a4a0cca
theaudaxproject <50760313+theaudaxproject@users.noreply.github.com>
2019-08-16 23:10:18 -0600

    Audaxify

--- client/component/Card/CardExchanges.jsx
+++ client/component/Card/CardExchanges.jsx
@@ -5,13 +5,7 @@ import Card from './Card';
 
 const CardExchanges = () => (
   <Card title="Exchanges">
-    <a href="https://crypto-bridge.org/" target="_blank" rel="nofollow noopener">CryptoBridge</a><br />
-    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank" rel="nofollow noopener">Txbit.io</a><br />
-    <a href="https://blocknet.co/" target="_blank" rel="nofollow noopener">Blocknet</a><br />
-    <a href="https://www.altilly.com/market/BWK_BTC" target="_blank" rel="nofollow noopener">altilly</a><br />
-    <a href="https://bastionex.com/" target="_blank" rel="nofollow noopener">Bastion</a><br />
-    <a href="https://green-dex.net/market/GREENDEX.BWK_GREENDEX.BTC" target="_blank" rel="nofollow noopener">GreenDex</a><br />
-    <a href="https://novaexchange.com/market/BTC_BWK/" target="_blank" rel="nofollow noopener">NovaExchange</a><br />
+    <a href="https://crex24.com/exchange/AUDAX-BTC" target="_blank" rel="nofollow noopener">Crex24</a><br />
   </Card>
 );
 
